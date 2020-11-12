0153977aa1128ea284c535af589f1c124f7bab2d
martinsione <sionemartin@hotmail.com>
2020-11-12 20:45:07 -0300

    added prevent focus shifting while mode=fullscreen.

--- local/.local/src/dwm/config.h
+++ local/.local/src/dwm/config.h
@@ -83,7 +83,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_o,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_o,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
@@ -95,8 +95,8 @@ static Key keys[] = {
 	/* { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, */
 	/* { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, */
 	/* { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, */
-	/* { MODKEY,                       XK_space,  setlayout,      {0} }, */
-	/* { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} }, */
+	{ MODKEY|ControlMask,           XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,			            XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	/* { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } }, */
