b4999e82649973cd82129ac3997048ac58d99a1d
martinsione <sionemartin@hotmail.com>
2020-11-12 20:52:38 -0300

    added alacritty to the swallow mode.

--- local/.local/src/dwm/config.def.h
+++ local/.local/src/dwm/config.def.h
@@ -33,12 +33,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class    instance    title       tags mask     isfloating   monitor */
-	{ "discord", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "zoom",	 NULL,     NULL,           1 << 1,    0,          0,          -1,        -1 },
-	{ "Gimp",    NULL,     NULL,           1 << 4,    0,          0,           0,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class		instance    title       tags mask     isfloating   monitor */
+	{ "discord",	NULL,		NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "zoom",		NULL,		NULL,           1 << 1,    0,          0,          -1,        -1 },
+	{ "Gimp",		NULL,		NULL,           1 << 4,    0,          0,           0,        -1 },
+	{ "St",			NULL,		NULL,           0,         0,          1,           0,        -1 },
+	{ "alacritty",  NULL,		NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,			NULL,     "Event Tester",	0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
