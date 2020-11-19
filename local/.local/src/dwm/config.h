5652bd07c9e0f36c749f216424f51d9c9db728f5
martinsione <sionemartin@hotmail.com>
2020-11-19 16:51:21 -0300

    changed gaps, font-size, and layouts (letters).

--- local/.local/src/dwm/config.h
+++ local/.local/src/dwm/config.h
@@ -6,14 +6,14 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static int smartgaps                = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"UbuntuMono Nerd Font:size=11:weight=bold:antialias=true:autohint:true", "JoyPixels:size=8:antialias=true:autohint=true"};
-static const char dmenufont[]       = "UbuntuMono Nerd Font:size=11:weight=bold:antialias=true:autohint:true";
+static const char *fonts[]          = {"UbuntuMono Nerd Font:size=12:antialias=true:autohint:true", "JoyPixels:size=8:antialias=true:autohint=true"};
+static const char dmenufont[]       = "UbuntuMono Nerd Font:size=12:antialias=true:autohint:true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -63,20 +63,20 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "|  []=  |",      tile },    /* first entry is default */
-	{ "|  [M]  |",      monocle },
-	{ "|  [@]  |",      spiral },
-	{ "|  [\\]  |",     dwindle },
-	{ "|  H[]  |",      deck },
-	{ "|  TTT  |",      bstack },
-	{ "|  ===  |",      bstackhoriz },
-	{ "|  HHH  |",      grid },
-	{ "|  ###  |",      nrowgrid },
-	{ "|  ---  |",      horizgrid },
-	{ "|  :::  |",      gaplessgrid },
-	{ "|  |M|  |",      centeredmaster },
-	{ "|  >M>  |",      centeredfloatingmaster },
-	{ "|  ><>  |",      NULL },    /* no layout function means floating behavior */
+	{ "[]= ",      tile },    /* first entry is default */
+	{ "[M] ",      monocle },
+	{ "[@] ",      spiral },
+	{ "[\\] ",     dwindle },
+	{ "H[] ",      deck },
+	{ "TTT ",      bstack },
+	{ "=== ",      bstackhoriz },
+	{ "HHH ",      grid },
+	{ "### ",      nrowgrid },
+	{ "--- ",      horizgrid },
+	{ "::: ",      gaplessgrid },
+	{ "|M| ",      centeredmaster },
+	{ ">M> ",      centeredfloatingmaster },
+	{ "><> ",      NULL },    /* no layout function means floating behavior */
 	{ NULL,       NULL },
 
     // Esthetic layouts
