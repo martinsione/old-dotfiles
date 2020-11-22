a46dcb34dfbc3c443fc2f1e87feb7cf50a908541
martinsione <sionemartin@hotmail.com>
2020-11-22 15:23:49 -0300

    corrected a typo.

--- local/.local/src/dwm/config.h
+++ local/.local/src/dwm/config.h
@@ -42,7 +42,7 @@ static const unsigned int alphas[][3]      = {
 
 /* tagging */
 /* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
-static const char *tags[] = { "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   " };
+static const char *tags[] = { "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   " };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -55,7 +55,7 @@ static const Rule rules[] = {
 	{ "Thunderbird",	NULL,     NULL,           1 << 4,		0,          0,          -1,        -1 },
 	{ "Gimp",			NULL,     NULL,           1 << 5,		1,          0,           0,        -1 },
 	{ "discord",		NULL,     NULL,           1 << 7,		0,          0,          -1,        -1 },
-	{ "steam",			NULL,     NULL,           1 << 8,		0,          0,          -1,        -1 },
+	{ "Steam",			NULL,     NULL,           1 << 8,		0,          0,          -1,        -1 },
 	{ "St",				NULL,     NULL,           0,			0,          1,           0,        -1 },
 	{ "Alacritty",		NULL,     NULL,           0,			0,          1,           0,        -1 },
 	{ NULL,				NULL,     "Event Tester", 0,			0,          0,           1,        -1 }, /* xev */
