e302045f018e2099cddb17438715c44eb90256df
martin <martin@arch.localdomain>
2020-12-13 01:11:19 -0300

    added script to auto install aur packages

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort backups/arch-packages/pacman.list )) &&
+yay -S --needed $(comm -12 <(yay -Slq | sort) <(sort backups/arch-packages/yay.list )) &&
 mkdir -p ~/.config/VSCodium/User ~/.local &&
 stow linux &&
 cd ~/.local/src/dwm && make && sudo make clean install &&
 cd ~/.local/src/dwmblocks && make && sudo make clean install &&
 cd ~/.local/src/st && make && sudo make clean install &&
+chsh -s $(which zsh) &&
 . ~/.zprofile
